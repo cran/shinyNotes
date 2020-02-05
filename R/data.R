df145317234ae201638fd01ee84da8a9ff60a66d
Daniel Kovtun <quantumfusetrader@gmail.com>
2020-02-05 15:30:05 +0000

    version 0.0.1

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Demo notes for testing \code{shinynote} module.
+#'
+#' A dataset containing package functions and their titles for the \code{shiny}, \code{shinyWidgets} and \code{dplyr} packages.
+#' Formatted in a structure compatible with the \code{shinyNotes::shinynotes} module.
+#'
+#' @format A \code{tibble} with 274 rows and 3 variables:
+#' \describe{
+#'   \item{package}{package title, character class}
+#'   \item{category}{function name, character class}
+#'   \item{update}{function title, character class}
+#'   ...
+#' }
+#' @source \href{https://CRAN.R-project.org/package=shiny}{shiny help pages}
+#' @source \href{https://CRAN.R-project.org/package=shinyWidgets}{shinyWidgets help pages}
+#' @source \href{https://CRAN.R-project.org/package=dplyr}{dplyr help pages}
+"demo_notes"
